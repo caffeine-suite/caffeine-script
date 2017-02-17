bf097d4877020dad1ac3b39fdab4d4d2cfa7f3c3
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-16 17:19:38 -0700

    REFACTOR - converting more to caf ALL 641 TESTS PASSING

--- cafInCaf/CaffeineScript/CaffeineScript.js
+++
@@ -1,2 +0,0 @@
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return {compile: function(source) {let Parser = require('./Parser'); return {compiled: {js: (Parser).parse(source).getStn().transform().toJsModule()}};}};});
--- source/CaffeineScript/CaffeineScript.coffee
+++
@@ -1,4 +0,0 @@
-Parser = require './Parser'
-module.exports =
-  compile: (source, options) ->
-    compiled: js: Parser.parse(source, options).getStn().transform().toJsModule()
