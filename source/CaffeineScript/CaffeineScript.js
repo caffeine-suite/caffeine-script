7dd1cf6dbd1c0ab3c60bb19dd9e0ceb5f28469f7
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-19 21:20:18 -0700

    CONVERSION: recompiles all source/CaffeineScript/* root .caf files ALL 696 TESTS PASSING

--- source/CaffeineScript/CaffeineScript.js
+++ source/CaffeineScript/CaffeineScript.js
@@ -1,13 +1,2 @@
-let Caf = require("caffeine-script-runtime");
-Caf.defMod(module, () => {
-  return {
-    compile: function(source, options) {
-      let Parser = require("./Parser");
-      return {
-        compiled: {
-          js: Parser.parse(source, options).getStn().transform().toJsModule()
-        }
-      };
-    }
-  };
-});
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return {compile: function(source, options) {let Parser = require('./Parser'); return {compiled: {js: (Parser).parse(source, options).getStn().transform().toJsModule()}};}};});
--- source/CaffeineScript/Lib.js
+++ source/CaffeineScript/Lib.js
@@ -1,18 +1,2 @@
-let Caf = require("caffeine-script-runtime");
-Caf.defMod(module, () => {
-  let RegExp = global.RegExp;
-  return {
-    deescapeSpaces: function(string) {
-      return string.replace(/\\ /g, " ");
-    },
-    escapeNewLines: function(string) {
-      return string.replace(/\n/g, "\\n");
-    },
-    escapeUnEscapedQuotes: function(string, quote = '"') {
-      return string.replace(
-        RegExp(`([^${quote}\\\\]*(?:\\\\.[^${quote}\\\\]*)*)${quote}`, "g"),
-        `$1${quote}`
-      );
-    }
-  };
-});
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {let RegExp = global.RegExp; return {deescapeSpaces: function(string) {return string.replace(/\\ /g, " ");}, escapeNewLines: function(string) {return string.replace(/\n/g, "\\n");}, escapeUnEscapedQuotes: function(string, quote = '"') {return string.replace(RegExp(`([^${quote}\\\\]*(?:\\\\.[^${quote}\\\\]*)*)${quote}`, "g"), `$1${quote}`);}};});
