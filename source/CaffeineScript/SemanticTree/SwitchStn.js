a395e59caa68da385e66b647d2ceb1c67c43db52
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-20 20:16:15 -0700

    CONVERTED: SwitchStn SwitchWhenStn ALL 721 PASSING

---
+++ source/CaffeineScript/SemanticTree/SwitchStn.js
@@ -0,0 +1,43 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"), BaseStn = require("./BaseStn");
+  return SwitchStn = Caf.defClass(
+    class SwitchStn extends BaseStn {},
+    function() {
+      this.prototype.toJs = function() {
+        let condition, switchWhens, switchElse, falsifyCases, options, cases;
+        ({ condition, switchWhens, switchElse } = this.labeledChildren);
+        falsifyCases = !condition;
+        options = { falsifyCases: falsifyCases };
+        cases = Caf.e(switchWhens, [], (clause, k, into) => {
+          return into.push(clause.toJs(options));
+        });
+        if (switchElse) {
+          cases.push(`default: ${switchElse.toJs()}`);
+        }
+        return `switch (${this.getConditionJs()}) {${cases.join(" break; ")}}`;
+      };
+      this.prototype.toJsExpression = function() {
+        let condition, switchWhens, switchElse, falsifyCases, options, cases;
+        ({ condition, switchWhens, switchElse } = this.labeledChildren);
+        falsifyCases = !condition;
+        options = { falsifyCases: falsifyCases };
+        cases = Caf.e(switchWhens, [], (clause, k, into) => {
+          return into.push(clause.toFunctionBodyJs(options));
+        });
+        if (switchElse) {
+          cases.push(`default: ${switchElse.toFunctionBodyJs()}`);
+        }
+        return `(() => {switch (${this.getConditionJs()}) {${cases.join(
+          " "
+        )}};})()`;
+      };
+      this.prototype.getConditionJs = function() {
+        let condition;
+        ({ condition } = this.labeledChildren);
+        return condition ? condition.toJsExpression() : "false";
+      };
+      return this;
+    }
+  );
+});
---
+++ source/CaffeineScript/SemanticTree/SwitchWhenStn.js
@@ -0,0 +1,33 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"), BaseStn = require("./BaseStn");
+  return SwitchWhenStn = Caf.defClass(
+    class SwitchWhenStn extends BaseStn {},
+    function() {
+      this.prototype.toJs = function(options) {
+        let thenDo;
+        ({ thenDo } = this.labeledChildren);
+        return `${this.getCasesJs(options)}: ${thenDo.toJs()};`;
+      };
+      this.prototype.getCasesJs = function(options) {
+        let falsifyCases, whenValue, cases;
+        ({ falsifyCases } = options);
+        ({ whenValue } = this.labeledChildren);
+        cases = whenValue.implicitArray
+          ? Caf.e(whenValue.children, [], (m, k, into) => {
+              return into.push(m.toJsExpression());
+            })
+          : [whenValue.toJsExpression()];
+        return falsifyCases
+          ? `case !(${cases.join("): case !(")})`
+          : `case ${cases.join(": case ")}`;
+      };
+      this.prototype.toFunctionBodyJs = function(options) {
+        let thenDo;
+        ({ thenDo } = this.labeledChildren);
+        return `${this.getCasesJs(options)}: ${thenDo.toFunctionBodyJs()};`;
+      };
+      return this;
+    }
+  );
+});
