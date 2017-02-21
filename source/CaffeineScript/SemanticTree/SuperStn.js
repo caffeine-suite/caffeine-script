8c3cfb491766dfe90d8c400438a19ef67ba66e16
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-20 22:44:56 -0700

    CONVERT: SuperStn ALL 724 PASSING

---
+++ source/CaffeineScript/SemanticTree/SuperStn.js
@@ -0,0 +1,54 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"),
+    BaseStn = require("./BaseStn"),
+    Error,
+    merge;
+  ({ Error, merge } = Caf.i(["Error", "merge"], [ArtFoundation, global]));
+  return SuperStn = Caf.defClass(
+    class SuperStn extends BaseStn {
+      constructor(props, args) {
+        super(...arguments);
+        this.args = args;
+        this.args.length === 1 && this.args[0].props.implicitArray
+          ? this.args = this.args[0].children
+          : undefined;
+      }
+    },
+    function() {
+      this.prototype.needsParens = false;
+      this.prototype.transform = function() {
+        let propValue, methodName, m, __;
+        if (!(propValue = this.findParent("ObjectPropValue"))) {
+          throw new Error("super must be used inside an object-literal value");
+        }
+        methodName = propValue.labeledChildren.propName.props.value;
+        if (m = methodName.match(/^@(.*)/)) {
+          [__, methodName] = m;
+        }
+        return new this.class(
+          merge(this.props, { methodName: methodName }),
+          this.transformChildren()
+        );
+      };
+      this.prototype.toJs = function() {
+        let args, method;
+        ({ args } = this);
+        return this.props.calledInConstructor
+          ? (args = this.props.passArguments
+              ? ["...arguments"]
+              : Caf.e(args, [], (a, k, into) => {
+                  return into.push(a.toJsExpression());
+                }), `super(${args.join(", ")})`)
+          : (method = this.props.passArguments
+              ? (args = "arguments", "apply")
+              : (args = Caf.e(args, [], (a, k, into) => {
+                  return into.push(a.toJsExpression());
+                }), "call"), `Caf.getSuper(this).${this.props.methodName}.${method}${this.applyRequiredParens(
+              ["this"].concat(args).join(", ")
+            )}`);
+      };
+      return this;
+    }
+  );
+});
