d5e646bee9ca9783c07ef1a4c99e5936911a5088
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-20 23:54:41 -0700

    CONVERT: RootStn ALL 725 PASSING

---
+++ cafInCaf/CaffeineScript/SemanticTree/RootStn.js
@@ -0,0 +1,63 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"),
+    StatementsStn = require("./StatementsStn"),
+    ScopeStnMixin = require("scope-stn-mixin"),
+    BaseStn = require("./BaseStn"),
+    RootStn,
+    compactFlatten;
+  ({ RootStn, compactFlatten } = Caf.i(["RootStn", "compactFlatten"], [
+    ArtFoundation,
+    global
+  ]));
+  StatementsStn;
+  return RootStn = Caf.defClass(
+    class RootStn extends ScopeStnMixin(BaseStn) {
+      constructor(props, children) {
+        super(...arguments);
+        this.statements = children[0];
+      }
+    },
+    function() {
+      this.prototype.cloneWithNewStatements = function(statements) {
+        return new RootStn(this.props, [
+          StatementsStn(compactFlatten(statements))
+        ]);
+      };
+      this.prototype.transform = function() {
+        let ret;
+        ret = Caf.getSuper(this).transform.apply(this, arguments);
+        ret.updateScope(ret);
+        return ret;
+      };
+      this.prototype.toJsModule = function() {
+        let identifiersUsedButNotAssigned, statementsJs, lets, statements;
+        ({ identifiersUsedButNotAssigned } = this);
+        identifiersUsedButNotAssigned = Caf.e(
+          identifiersUsedButNotAssigned,
+          [],
+          (k, v, into) => {
+            return into.push(`${k} = global.${k}`);
+          }
+        );
+        statementsJs = this.statements.toFunctionBodyJs();
+        lets = compactFlatten([
+          identifiersUsedButNotAssigned,
+          this.requiredIdentifierLets
+        ]);
+        statements = compactFlatten([
+          lets.length > 0 ? `let ${lets.join(", ")}` : undefined,
+          statementsJs
+        ]);
+        return "let Caf = require('caffeine-script-runtime');\nCaf.defMod(module, () => {#{statements.join '; '};});";
+      };
+      this.prototype.toJs = function() {
+        let statementsJs;
+        statementsJs = this.statements.toJs();
+        return compactFlatten([this.getAutoLets(), statementsJs]).join("; ") +
+          ";";
+      };
+      return this;
+    }
+  );
+});
---
+++ source/CaffeineScript/SemanticTree/RootStn.js
@@ -0,0 +1,66 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"),
+    StatementsStn = require("./StatementsStn"),
+    ScopeStnMixin = require("./ScopeStnMixin"),
+    BaseStn = require("./BaseStn"),
+    RootStn,
+    compactFlatten;
+  ({ RootStn, compactFlatten } = Caf.i(["RootStn", "compactFlatten"], [
+    ArtFoundation,
+    global
+  ]));
+  StatementsStn;
+  return RootStn = Caf.defClass(
+    class RootStn extends ScopeStnMixin(BaseStn) {
+      constructor(props, children) {
+        super(...arguments);
+        this.statements = children[0];
+      }
+    },
+    function() {
+      this.prototype.cloneWithNewStatements = function(statements) {
+        return new RootStn(this.props, [
+          StatementsStn(compactFlatten(statements))
+        ]);
+      };
+      this.prototype.transform = function() {
+        let ret;
+        ret = Caf.getSuper(this).transform.apply(this, arguments);
+        ret.updateScope(ret);
+        return ret;
+      };
+      this.prototype.toJsModule = function() {
+        let identifiersUsedButNotAssigned, statementsJs, lets, statements;
+        ({ identifiersUsedButNotAssigned } = this);
+        identifiersUsedButNotAssigned = Caf.e(
+          identifiersUsedButNotAssigned,
+          [],
+          (v, k, into) => {
+            return into.push(`${k} = global.${k}`);
+          }
+        );
+        statementsJs = this.statements.toFunctionBodyJs();
+        lets = compactFlatten([
+          identifiersUsedButNotAssigned,
+          this.requiredIdentifierLets
+        ]);
+        statements = compactFlatten([
+          lets.length > 0 ? `let ${lets.join(", ")}` : undefined,
+          statementsJs
+        ]);
+        return `\
+let Caf = require('caffeine-script-runtime');\n\
+Caf.defMod(module, () => {${statements.join("; ")};});\
+`;
+      };
+      this.prototype.toJs = function() {
+        let statementsJs;
+        statementsJs = this.statements.toJs();
+        return compactFlatten([this.getAutoLets(), statementsJs]).join("; ") +
+          ";";
+      };
+      return this;
+    }
+  );
+});
