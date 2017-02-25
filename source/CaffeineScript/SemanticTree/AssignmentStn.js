0353693ccd680c79a19e7c9a0e1fbdd3081ba99e
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-02-25 13:00:25 -0700

    CONVERTED AssignmentStn - no new bugs! ALL 772 PASSING

---
+++ source/CaffeineScript/SemanticTree/AssignmentStn.js
@@ -0,0 +1,62 @@
+let Caf = require("caffeine-script-runtime");
+Caf.defMod(module, () => {
+  let ArtFoundation = require("art-foundation"),
+    BinaryOperatorStn = require("./BinaryOperatorStn"),
+    IdentifierStn = require("./IdentifierStn"),
+    ReferenceStn = require("./ReferenceStn"),
+    ArrayStn = require("./ArrayStn"),
+    SemanticTree,
+    supportedOperatorsRegExp,
+    ValueBaseCaptureStn = require("./ValueBaseCaptureStn");
+  BinaryOperatorStn;
+  IdentifierStn;
+  ReferenceStn;
+  ArrayStn;
+  SemanticTree = require("./namespace");
+  supportedOperatorsRegExp = /^([-+*\/%]|)$/;
+  return AssignmentStn = Caf.defClass(
+    class AssignmentStn extends ValueBaseCaptureStn {
+      constructor(props, children) {
+        super(...arguments);
+        this.operator = props.operator || "";
+        this.lValue = children[0];
+        this.rValue = children[1];
+      }
+    },
+    function() {
+      this.prototype.updateScope = function(scope) {
+        this.scope = scope;
+        if (this.lValue.type === "Reference") {
+          this.scope.addIdentifierAssigned(this.lValue.toJs());
+        }
+        return Caf.getSuper(this).updateScope.apply(this, arguments);
+      };
+      this.prototype.transform = function() {
+        return Caf.getSuper(this)
+          .transform.apply(this, arguments)
+          .postSuperTransform();
+      };
+      this.prototype.postSuperTransform = function() {
+        let value1, value2;
+        return !this.operator.match(supportedOperatorsRegExp)
+          ? ({ value1, value2 } = this.getValueWithBaseCapture(
+              this.lValue
+            ), BinaryOperatorStn(
+              { operator: this.operator },
+              value1,
+              SemanticTree.AssignmentStn(value2, this.rValue)
+            ))
+          : this;
+      };
+      this.prototype.toJs = function() {
+        return this.operator.match(supportedOperatorsRegExp)
+          ? `${this.lValue.toJs()} ${this.operator}= ${this.rValue.toJsExpression()}`
+          : `${this.lValue.toJsExpression()} ${this.operator} ${this.lValue.toJs()} = ${this.rValue.toJsExpression()}`;
+      };
+      this.prototype.toJsParenExpression = function() {
+        return `(${this.toJs()})`;
+      };
+      return this;
+    }
+  );
+});
