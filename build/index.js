9b6dae778ad908a6251e79cb83256d78bfe08a87
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-03-15 17:14:46 -0700

    build stable

--- build/index.js
+++ build/index.js
@@ -2121,7 +2121,7 @@ __webpack_require__(109);
 /* 29 */
 /***/ (function(module, exports) {
 
-module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","config":{"blanket":{"pattern":"source"}},"dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-object-tree-factory":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-eight":"*","caffeine-mc":"*","caffeine-script":"*","caffeine-script-runtime":"*","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.1","glob":"^7.1.2","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"CaffeineScript makes programming more wonderful, code more beautiful and programmers more productive. It is a lean, high-level language that empowers you to get the most out of any JavaScript runtime.","license":"ISC","name":"caffeine-script","repository":{"type":"git","url":"git@github.com:shanebdavis/caffeine-script.git"},"scripts":{"build":"caf -v -p -C -c cafInCaf -o source","perf":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register perf","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"0.50.3"}
+module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","config":{"blanket":{"pattern":"source"}},"dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-object-tree-factory":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-eight":"*","caffeine-mc":"*","caffeine-script":"*","caffeine-script-runtime":"*","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.1","glob":"^7.1.2","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"CaffeineScript makes programming more wonderful, code more beautiful and programmers more productive. It is a lean, high-level language that empowers you to get the most out of any JavaScript runtime.","license":"ISC","name":"caffeine-script","repository":{"type":"git","url":"git@github.com:shanebdavis/caffeine-script.git"},"scripts":{"build":"caf -v -p -C -c cafInCaf -o source","perf":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register perf","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"0.51.0"}
 
 /***/ }),
 /* 30 */
@@ -2435,7 +2435,7 @@ Caf.defMod(module, () => {
       );
       this.rule({
         comprehensionValueClause: [
-          "_? comprehensionValueClauseType _? value:expressionWithOneLessBlockOrBlock",
+          "_? comprehensionValueClauseType _? value:keywordLabeledStatementsWithOneLessBlock",
           {
             stnFactory: "ComprehensionValueClauseStn",
             stnProps: function() {
@@ -2443,11 +2443,7 @@ Caf.defMod(module, () => {
             }
           }
         ],
-        comprehensionValueClauses: "comprehensionValueClause+",
-        expressionWithOneLessBlockOrBlock: [
-          "expressionWithOneLessBlock",
-          "requiredValue"
-        ]
+        comprehensionValueClauses: "comprehensionValueClause+"
       });
       this.rule(
         {
@@ -2459,8 +2455,8 @@ Caf.defMod(module, () => {
       this.rule({
         optionalArg: "_comma_? !withOrDo argDef",
         comprehensionIterationTypeClause_: "comprehensionIterationType _?",
-        comprehensionIterable: "expressionWithOneLessBlockOrBlock",
-        comprehensionWith: "_? withOrDo _ lineOfStatementsOrBlock",
+        comprehensionIterable: "keywordLabeledStatementsWithOneLessBlock",
+        comprehensionWith: "_? withOrDo _? lineOfStatementsOrBlock",
         comprehensionBody: ["block", "comprehensionWith"]
       });
       return this.rule(
@@ -2664,6 +2660,7 @@ Caf.defMod(module, () => {
         ({ matchBlock } = Extensions.IndentBlocks),
         (upToButNotEol = /[^\n]*/y),
         function() {
+          let oneLessBlockSubparser;
           this.rule({
             lineStartExpression: "multilineImplicitObject",
             implicitArrayOrExpression: ["implicitArray", "expression"],
@@ -2707,45 +2704,52 @@ Caf.defMod(module, () => {
             { throwExpression: "throw _ expressionWithoutBinOps" },
             { stnFactory: "ThrowStn" }
           );
+          oneLessBlockSubparser = rule => {
+            return function(parentNode) {
+              let nextOffset,
+                source,
+                offset,
+                originalOffset,
+                match,
+                m,
+                endOffset,
+                matchLength,
+                expressionSource;
+              ({ nextOffset, source } = parentNode);
+              offset = nextOffset;
+              originalOffset = offset;
+              upToButNotEol.lastIndex = offset;
+              return (match = upToButNotEol.exec(source))
+                ? (([m] = match),
+                  (endOffset = offset += m.length),
+                  (() => {
+                    while ((match = matchBlock(source, offset))) {
+                      endOffset = offset;
+                      ({ matchLength } = match);
+                      offset += matchLength;
+                    }
+                  })(),
+                  (expressionSource = source.slice(originalOffset, endOffset)),
+                  parentNode.subparse(expressionSource, {
+                    allowPartialMatch: true,
+                    rule,
+                    originalOffset,
+                    originalMatchLength: endOffset - originalOffset
+                  }))
+                : undefined;
+            };
+          };
           return this.rule({
             expressionWithOneLessBlock: {
-              parse: function(parentNode) {
-                let nextOffset,
-                  source,
-                  offset,
-                  originalOffset,
-                  match,
-                  m,
-                  endOffset,
-                  matchLength,
-                  expressionSource;
-                ({ nextOffset, source } = parentNode);
-                offset = nextOffset;
-                originalOffset = offset;
-                upToButNotEol.lastIndex = offset;
-                return (match = upToButNotEol.exec(source))
-                  ? (([m] = match),
-                    (endOffset = offset += m.length),
-                    (() => {
-                      while ((match = matchBlock(source, offset))) {
-                        endOffset = offset;
-                        ({ matchLength } = match);
-                        offset += matchLength;
-                      }
-                    })(),
-                    (expressionSource = source.slice(
-                      originalOffset,
-                      endOffset
-                    )),
-                    parentNode.subparse(expressionSource, {
-                      allowPartialMatch: true,
-                      rule: "implicitArrayOrExpression",
-                      originalOffset,
-                      originalMatchLength: endOffset - originalOffset
-                    }))
-                  : undefined;
-              }
-            }
+              parse: oneLessBlockSubparser("implicitArrayOrExpression")
+            },
+            lineOfStatementsWithOneLessBlock: {
+              parse: oneLessBlockSubparser("lineOfStatementsOrBlock")
+            },
+            keywordLabeledStatementsWithOneLessBlock: [
+              "lineOfStatementsWithOneLessBlock",
+              "statementBlock"
+            ]
           });
         }
       );
